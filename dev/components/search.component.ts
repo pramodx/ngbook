b8e46b214fc1124b8eae53307d794513b070075a
Pramod Nair <pramod.nair@open.edu.au>
2016-06-14 17:07:50 +1000

    all modules working now. over to testing next

--- dev/components/search.component.ts
+++ dev/components/search.component.ts
@@ -27,7 +27,7 @@ import {SpotifyService} from "../services/spotify.service";
 					<div class="col-sm-6 col-md-4" *ngFor="let t of results">
 						<div class="thumbnail">
 							<div class="content">
-								<img src="{{t.album.images[0].url}}" class="img-responsive">
+								<img src="{{t.album.images[1].url}}" class="img-responsive">
 								<div class="caption">
 									<h3>
 										<a [routerLink]="['/Artists', {id: t.artists[0].id}]">{{ t.artists[0].name }}</a>
--- dev/components/track.component.ts
+++ dev/components/track.component.ts
@@ -1,3 +1,47 @@
-export class TrackComponent {
+import {CORE_DIRECTIVES, LocationStrategy} from "@angular/common";
+import {OnInit, Component} from "@angular/core";
+import {SpotifyService} from "../services/spotify.service";
+import {RouteParams} from "@angular/router-deprecated";
 
+@Component({
+	selector: 'theTrack',
+	directives: [CORE_DIRECTIVES],
+	template: `
+  <div *ngIf="track">
+    <h1>{{ track.name }}</h1>
+
+    <p>
+      <img src="{{ track.album.images[1].url }}">
+    </p>
+
+    <p>
+      <audio controls src="{{ track.preview_url }}"></audio>
+    </p>
+
+    <p><a href (click)="back()">Back</a></p>
+  </div>
+  `
+})
+export class TrackComponent implements OnInit {
+	id: string;
+	track: Object;
+
+	constructor(public routeParams: RouteParams, public spotify: SpotifyService,
+				public locationStrategy: LocationStrategy) {
+		this.id = routeParams.get('id');
+	}
+
+	ngOnInit(): void {
+		this.spotify
+			.getTrack(this.id)
+			.subscribe((res: any) => this.renderTrack(res));
+	}
+
+	back(): void {
+		this.locationStrategy.back();
+	}
+
+	renderTrack(res: any): void {
+		this.track = res;
+	}
 }
