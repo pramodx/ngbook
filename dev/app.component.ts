c3412ffd23f3155d412207057b59e26da0eefef3
Pramod Nair <pramod.nair@open.edu.au>
2016-06-14 17:16:44 +1000

    moved app to its own component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,2 +1,26 @@
 
 
+import {ROUTER_DIRECTIVES, RouteConfig, Router} from "@angular/router-deprecated";
+import {SearchComponent} from "./components/search.component";
+import {ArtistComponent} from "./components/artist.component";
+import {TrackComponent} from "./components/track.component";
+import {AlbumComponent} from "./components/album.component";
+import {Component} from "@angular/core";
+@Component({
+	selector: 'router-app',
+	directives: [ROUTER_DIRECTIVES],
+	template: `<router-outlet></router-outlet>`
+})
+@RouteConfig([
+	{path: '/', name: 'root', redirectTo: ['Search']},
+	{path: '/search', name: 'Search', component: SearchComponent },
+	{ path: '/artists/:id', name: 'Artists', component: ArtistComponent },
+	{ path: '/tracks/:id', name: 'Tracks', component: TrackComponent },
+	{ path: '/albums/:id', name: 'Albums', component: AlbumComponent }
+])
+export class AppComponent {
+	query: string;
+	constructor(public router: Router){
+
+	}
+}
--- dev/boot.ts
+++ dev/boot.ts
@@ -1,7 +1,7 @@
 ///<reference path="../typings/browser.d.ts"/>
-import {Component, provide} from "@angular/core";
+import { provide} from "@angular/core";
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { ROUTER_DIRECTIVES, ROUTER_PROVIDERS, ROUTER_PRIMARY_COMPONENT, Router, RouteConfig } from "@angular/router-deprecated";
+import { ROUTER_PROVIDERS, ROUTER_PRIMARY_COMPONENT } from "@angular/router-deprecated";
 
 import {HTTP_PROVIDERS} from "@angular/http";
 import {APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from "@angular/common";
@@ -11,34 +11,9 @@ import {APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from "@angular/co
 */
 import {SPOTIFY_PROVIDERS} from "./services/spotify.service";
 
+import {AppComponent} from "./app.component";
 
 
-/*
-COMPONENTS
-*/
-import {SearchComponent} from "./components/search.component";
-import {AlbumComponent} from "./components/album.component";
-import {ArtistComponent} from "./components/artist.component";
-import {TrackComponent} from "./components/track.component";
-
-@Component({
-	selector: 'router-app',
-	directives: [ROUTER_DIRECTIVES],
-	template: `<router-outlet></router-outlet>`
-})
-@RouteConfig([
-	{path: '/', name: 'root', redirectTo: ['Search']},
-	{path: '/search', name: 'Search', component: SearchComponent },
-	{ path: '/artists/:id', name: 'Artists', component: ArtistComponent },
-	{ path: '/tracks/:id', name: 'Tracks', component: TrackComponent },
-	{ path: '/albums/:id', name: 'Albums', component: AlbumComponent }
-])
-class AppComponent {
-	query: string;
-	constructor(public router: Router){
-
-	}
-}
 bootstrap(AppComponent, [
 	ROUTER_PROVIDERS,
 	HTTP_PROVIDERS,
